bd92b8ef0e8c4b9cbe8501b7f7a7a1f010ff91a5
Ignaslev <levinskas.ignas@gmail.com>
2025-03-18 17:06:33 +0200

    Added railway to allowed hosts

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app",
+]
 if DEBUG:
     ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
 
