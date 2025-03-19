6d344de1f86d79f89edf12c446d379185a9ecd63
Ignaslev <levinskas.ignas@gmail.com>
2025-03-19 11:02:54 +0200

    fixing allowed hosts to be able to log in and register

--- main/settings.py
+++ main/settings.py
@@ -23,13 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
 ALLOWED_HOSTS = [
     ".railway.app",
+    'airy-grace-production.up.railway.app',
+    '127.0.0.1',
+    'localhost',
 ]
 if DEBUG:
     ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
